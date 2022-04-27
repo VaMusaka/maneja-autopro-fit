858af75d792dc15674b74ccf70cc6142e401e33b
Percy Musaka <pmusaka@instantia.co.uk>
2022-04-27 23:30:59 +0100

    Phone Validation

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -192,7 +192,7 @@ export const makeDropDownOptions = (data, labelField, valueField) => {
 
 export const isUkPhone = (phone) => {
     // eslint-disable-next-line
-    const regEx = /^(((\+44\s?|0044\s?)?|(\(?0))((2[03489]\)?\s?\d{4}\s?\d{4})|(1[23456789]1\)?\s?\d{3}\s?\d{4})|(1[23456789][234578][0234679]\)?\s?\d{6})|(1[2579][0245][0467]\)?\s?\d{5})|(11[345678]\)?\s?\d{3}\s?\d{4})|(1[35679][234689]\s?[46789][234567]\)?\s?\d{4,5})|([389]\d{2}\s?\d{3}\s?\d{4})|([57][0-9]\s?\d{4}\s?\d{4})|(500\s?\d{6})|(7[456789]\d{2}\s?\d{6})))$/gm;
+    const regEx = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
     return phone.match(regEx)
 }
 
